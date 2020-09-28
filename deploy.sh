50fd196c56321a68417010c1dd6a9c96424ad493
i2unix <70039335+i2unix@users.noreply.github.com>
2020-09-28 21:08:02 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 # Deploying Bash Script
+
+# Install Apache2.4 Web Server
+sudo apt update
+sudo apt install apache2
+
+# Add current user to server group "www-data"
+sudo chown -R $USER:www-data /var/www
