526edf6a4f1127f169fe3b2ee6553cec057d76b0
i2unix <70039335+i2unix@users.noreply.github.com>
2020-09-28 21:45:30 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,18 @@ sudo apt install apache2
 
 # Add current user to server group "www-data"
 sudo chown -R $USER:www-data /var/www
+
+#Install  Mysql Server
+sudo apt install mysql-server
+sudo mysql_secure_installation
+
+# Configure root mysql user
+sudo mysql
+
+mysql> SELECT user,authentication_string,plugin,host FROM mysql.user;
+mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+mysql> FLUSH PRIVILEGES;
+mysql> SELECT user,authentication_string,plugin,host FROM mysql.user;
+mysql> exit
+
+# Install PHP
