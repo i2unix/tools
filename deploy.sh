a470b28916a1d64a9d6bace00df81429b5b76532
i2unix <70039335+i2unix@users.noreply.github.com>
2020-09-28 22:38:22 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 
 # Install utils
-sudo apt install git mc curl -y
+sudo apt install git mc curl zip unzip -y
 
 # Install Apache2.4 Web Server
 sudo apt update
@@ -25,3 +25,31 @@ mysql> SELECT user,authentication_string,plugin,host FROM mysql.user;
 mysql> exit
 
 # Install PHP
+sudo add-apt-repository ppa:ondrej/php
+sudo update
+sudo apt upgrade
+sudo apt install php7.3 libapache2-mod-php7.3 php7.3-mysql php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
+
+# Configure Apache2 
+sudo nano /etc/apache2/mods-enabled/dir.conf
+
+<IfModule mod_dir.c>
+    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
+</IfModule>
+
+sudo systemctl restart apache2
+sudo service apache2 restart
+
+sudo systemctl status apache2
+sudo service apache2 status
+
+# Configure PHP
+
+
+# Install ZendServer
+
+# Install Composer
+
+# Install NodeJS
+
+
